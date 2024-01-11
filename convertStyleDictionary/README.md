056209b28f2de6725d88fc058b5cf0bd76623aad
정선정_UI기술개발팀 <sunjeong0420@sk.com>
2024-01-11 15:45:20 +0900

    feat: dropshadow convert 추가, css/scss format 수정

--- convertStyleDictionary/README.md
+++ convertStyleDictionary/README.md
@@ -3,6 +3,20 @@
 Figma Variables를 활용한 디자인과 UI개발에서 디자인 토큰 사용법
 style-dictionary 사용하여 json 가공 후 tokens.css 자동 생성
 
+## style-dictionary
+
+tailwindcss, sass, ESModules 에서 사용하기 위해 디자인 토큰(json) 가공하는 방법
+
+|                                | config.json transforms 데이터 가공                      |
+| ------------------------------ | ------------------------------------------------------- |
+| name/cti/kebab                 | css 또는 scss 변수명 사이 하이픈(-) 으로 연결           |
+| name/cti/constant              | css 또는 scss 변수명 모두 대문자로 변경                 |
+| name/cti/camel                 | css 또는 scss 변수명 사이 대문자 (단어 연결을 대문자로) |
+| name/cti/snake                 | css 또는 scss 변수명 사이 언더바(\_) 로 연결            |
+| custom/dimension               | css 또는 scss px -> rem 변환                            |
+| custom/typo                    | css 또는 scss typography 폰트 축약형                    |
+| StyleDictionary.registerFormat | css 변수명 format 수정                                  |
+
 ```bash
 npm install
 npm run build
@@ -11,14 +25,14 @@ npm run build
 ‼️ 사전에 설치해야 할 패키지
 
 ```
-npm install --save-dev style-dictionary sass
+npm install --save-dev style-dictionary sass @ctrl/tinycolor
 ```
 
 ## Getting Started
 
 ```bash
 .
-├── build/                     # 디자인 토큰 자동 생성
+├── build/                     # build 시 디자인 토큰 자동 생성
 │   └── tokens.css             # 해당 파일만 custom 가공
 │
 ├── src/                       # 작업폴더
@@ -31,13 +45,18 @@ npm install --save-dev style-dictionary sass
 └── README.md                  # 프로젝트 설명 문서
 ```
 
-### style-dictionary
+#### ./build.js
 
-tailwindcss, sass, ESModules 에서 사용하기 위해 디자인 토큰(json) 가공하는 방법
+**[type]**
+dimension : unit, spacing, radius
+color: color
+custom-fontStyle : typography
 
-|                   | config.json transforms 데이터 가공                      |
-| ----------------- | ------------------------------------------------------- |
-| name/cti/kebab    | css 또는 scss 변수명 사이 하이픈(-) 으로 연결           |
-| name/cti/constant | css 또는 scss 변수명 모두 대문자로 변경                 |
-| name/cti/camel    | css 또는 scss 변수명 사이 대문자 (단어 연결을 대문자로) |
-| name/cti/snake    | css 또는 scss 변수명 사이 언더바(\_) 로 연결            |
+**[category]**
+
+1. "primitive"
+   기준이 되는 collection
+2. "sementic"
+   기준 collection을 참조하는 collection
+3. custom-fontStyle
+   local style -> Text styles에서 생성하므로 category가 없다.
--- convertStyleDictionary/build.js
+++ convertStyleDictionary/build.js
@@ -1,4 +1,5 @@
 import StyleDictionary from "style-dictionary";
+import TinyColor from "@ctrl/tinycolor";
 
 StyleDictionary.registerTransform({
   name: "custom/dimension",
@@ -23,37 +24,79 @@ StyleDictionary.registerTransform({
   },
 });
 
+StyleDictionary.registerTransform({
+  name: "custom/shadow",
+  type: "value",
+  transitive: true,
+  matcher: (token) => {
+    return ["custom-shadow"].includes(token.type);
+  },
+  transformer: (token) => {
+    const tokenValue = token.value;
+    return `${tokenValue.shadowType === "innerShadow" ? "inset " : ""}${tokenValue.offsetX / 10}rem ${tokenValue.offsetY / 10}rem ${tokenValue.radius / 10}rem ${
+      tokenValue.spread / 10
+    }rem ${new TinyColor.TinyColor(tokenValue.color).toRgbString()}`;
+  },
+});
+
 const content = [];
 StyleDictionary.registerFormat({
-  name: "customFormat",
+  name: "customCssFormat",
   formatter: function ({ dictionary }) {
-    dictionary.allTokens.map((item) => {
-      if (item.type === "custom-fontStyle") {
-        content[item.name] = item.value;
-      } else if (item.type === "dimension") {
-        if (item.attributes.category === "sementic") {
-          const subKey = item.name.replace(/sementic_/g, "").trim();
-          const subValue = item.original.value;
-          const realValue = subValue
-            .replace(/{primitive.|}/g, "")
-            .trim()
-            .split(".")
-            .join("_");
-          content[subKey] = `var(--${realValue})`;
-        } else {
-          const key = item.name.replace(/primitive_/g, "").trim();
-          content[key] = `${item.original.value / 10}rem`;
-        }
-      } else if (item.type === "color") {
-      }
-    });
+    formatCustom(dictionary, "css");
 
     const cssString = Object.entries(content)
-      .map(([key, value]) => `  --${key}: ${value};`)
+      .map(([key, value]) => ` --${key}: ${value};`)
       .join("\n");
 
     return `:root {\n${cssString}\n}`;
   },
 });
+
+StyleDictionary.registerFormat({
+  name: "customScssFormat",
+  formatter: function ({ dictionary }) {
+    formatCustom(dictionary, "scss");
+
+    const cssString = Object.entries(content)
+      .map(([key, value]) => `$${key}: ${value};`)
+      .join("\n");
+
+    return `${cssString}\n`;
+  },
+});
+
+function formatCustom(dictionary, type) {
+  dictionary.allTokens.map((item) => {
+    if (item.type === "custom-fontStyle" || item.type === "custom-shadow") {
+      content[item.name] = item.value;
+    } else if (item.type === "color") {
+      // const key = item.name.replace(/primitive_/g, "").trim();
+      const key = item.name.split("_").splice(1).join("_");
+      content[key] = item.original.value;
+    } else if (item.type === "dimension") {
+      if (item.attributes.category === "sementic") {
+        // const subKey = item.name.replace(/sementic_/g, "").trim();
+        const subKey = item.name.split("_").splice(1).join("_");
+        const subValue = item.original.value;
+        const realValue = subValue
+          .replace(/{primitive.|}/g, "")
+          .trim()
+          .split(".")
+          .join("_");
+        if (type === "css") {
+          content[subKey] = `var(--${realValue})`;
+        } else if (type === "scss") {
+          content[subKey] = `$${realValue}`;
+        }
+      } else {
+        const key = item.name.split("_").splice(1).join("_");
+        // const key = item.name.replace(/primitive_/g, "").trim();
+        content[key] = `${item.original.value / 10}rem`;
+      }
+    }
+  });
+  return content;
+}
 
 StyleDictionary.extend("./config.json").cleanAllPlatforms().buildAllPlatforms();
